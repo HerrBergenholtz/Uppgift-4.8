8c22998244db44065a96fbcb283ef12f60125552
HerrBergenholtz <zbdv8800@gmail.com>
2023-10-26 23:45:12 +0200

    Fix

--- Uppgift 4.8/Program.cs
+++ Uppgift 4.8/Program.cs
@@ -8,22 +8,23 @@ namespace Uppgift_4._8
         {
             Console.WriteLine("Skriv in en sträng!");
             string sträng = Console.ReadLine();
-
+          
             Console.WriteLine("Skriv in exakt två tecken.");
             string karaktärer = Console.ReadLine();
-
+                
             karaktärer.Split("");
 
             string karaktär1 = karaktärer[0].ToString();
             string karaktär2 = karaktärer[1].ToString();
 
-            if(sträng.Contains(karaktär1) && sträng.Contains(karaktär2)) {
+            if (sträng.Contains(karaktär1) && sträng.Contains(karaktär2))
+            {
                 Console.WriteLine("Båda tecken finns i strängen");
             }
             else
             {
                 Console.WriteLine("Båda karaktärer ingår inte i strängen :(");
-            }
+            }    
             Console.ReadLine();
         }
     }
